e72096e78f19b9716979af50f242f5b3fcaf4978
Imran <imran1510043@gmail.com>
2022-04-15 15:39:25 +0600

    html and javascript portion done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Review from './components/Review';
+import reviews from './components/data';
 
-function App() {
+const App = () => {
+  const [id, setId] = useState(0);
+  const length = reviews.length;
+
+  const increment = () => {
+    setId((id + 1) % length)
+  }
+
+  const decrement = () => {
+    if(id === 0) {
+      setId(length - 1);
+    }
+    else {
+      setId((id - 1) % length)
+    }
+    
+  }
+
+  const surpriseMe = () => {
+    const randomNumber = Math.floor((Math.random() * length))
+    if(randomNumber === id) {
+      setId((id + 1) % length);
+    }
+    else {
+      setId(randomNumber);
+    }
+  }
+
   return (
     <main>
-      
+      <div>
+        <h2>Our Reviews</h2>
+        <div></div>
+      </div>
+      <article>
+        <Review id={id} increment={increment} decrement={decrement} surpriseMe={surpriseMe} />
+      </article>
     </main>
   );
 }
---
+++ src/components/Review.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import reviews from './data.js'
+
+const Review = ({ id, increment, decrement, surpriseMe }) => {
+    const { name, job, image, text } = reviews[id];
+  return (
+    <>
+        <img src={image} alt={name} />
+        <div>
+            <h4>{name}</h4>
+            <h4>{job}</h4>
+        </div>
+        <footer>
+            <p>{text}</p>
+            <div>
+                <button onClick={decrement}>{`<`}</button>
+                <button onClick={increment}>{`>`}</button>
+            </div>
+            <button onClick={surpriseMe}>Surprise Me</button>
+        </footer>
+    </>
+  )
+}
+
+export default Review
